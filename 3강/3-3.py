c3b3e67b18896943118e58135414ae92c7ac7188
jewoonlee <jewoonlee@hanyang.ac.kr>
2023-08-14 11:44:55 +0900

    3-3 2번째

--- 3강/3-3.py
+++ 3강/3-3.py
@@ -57,4 +57,81 @@ print(lr.score(train_poly,train_target))
 print(lr.score(test_poly,test_target))
 #이렇게 과하게 훈련할 경우에는 훈련세트에만 너무 과대적합 해짐으로 test 세트에는 안좋은 영향을 끼침
 
+# 과하게 훈련 하는것을 막기 위해서 규제라는 것을 함
+# 규제 a,b,c,d, 이런 것들을 작게 하는것
+# 규제를 하기전에 2장에 했던 스케일 정규화가 필요함
+# 2장에는 우리가 직접 했다면 이제는 사이킷 런에서 제공하는 standardscaler를 사용
+
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+ss.fit(train_poly)
+train_scaled = ss.transform(train_poly)
+test_scaled = ss.transform(test_poly)
+
+# 릿지(ridge): 계수를 제곱한 값 기준
+# 라쏘(lasso): 계수의 절대값
+
+# 릿지
+from sklearn.linear_model import Ridge
+ridge = Ridge()
+ridge.fit(train_scaled, train_target)
+print(ridge.score(test_scaled,test_target))
+
+# 규제의 정도를 조정할 수 있음: alpha가 커지면 규제 강도가 커지고, 작으면 규제 강도가 작아짐
+import matplotlib.pyplot as plt
+# train_score = []
+# test_score = []
+
+# alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
+# for alpha in alpha_list:
+#     ridge = Ridge(alpha=alpha)
+#     ridge.fit(train_scaled,train_target)
+#     train_score.append(ridge.score(train_scaled,train_target))
+#     test_score.append(ridge.score(test_scaled,test_target))
+
+# plt.plot(np.log10(alpha_list),train_score)
+# plt.plot(np.log10(alpha_list),test_score)
+# plt.xlabel('alpha')
+# plt.ylabel('R^2')
+# plt.show()
+
+ridge = Ridge(alpha= 0.1)
+ridge.fit(train_scaled,train_target)
+print(ridge.score(train_scaled,train_target))
+print(ridge.score(test_scaled,test_target))
+
+# 라쏘 회귀(lasso)
+from sklearn.linear_model import Lasso
+lasso = Lasso()
+lasso.fit(train_scaled,train_target)
+print(lasso.score(train_scaled,train_target))
+print(lasso.score(test_scaled,test_target))
+
+train_score = []
+test_score = []
+
+alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
+
+for alpha in alpha_list:
+    lasso = Lasso(alpha=alpha, max_iter=10000)
+    lasso.fit(train_scaled,train_target)
+    train_score.append(lasso.score(train_scaled,train_target))
+    test_score.append(lasso.score(test_scaled,test_target))
+
+print(train_score, test_score)
+
+plt.plot(np.log10(alpha_list),train_score)
+plt.plot(np.log10(alpha_list),test_score)
+plt.xlabel('alpha')
+plt.ylabel('R^2')
+plt.show()
+
+lasso = Lasso(alpha=10)
+lasso.fit(train_scaled,train_target)
+print(lasso.score(train_scaled,train_target))
+print(lasso.score(test_scaled,test_target))
+
+# 라쏘는 계수값이 0인 것이 있음
+print(np.sum(lasso.coef_ == 0)) # np.sum은 함수 배열을 다 더한 값
+
 
